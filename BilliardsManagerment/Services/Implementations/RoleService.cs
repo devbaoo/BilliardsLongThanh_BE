517f76fb04bd9bbc01d3787b6528c026a59d28f3
devbaoo_712 <caokhacbao0712@gmail.com>
2024-10-10 21:56:29 +0700

    first commit

---
+++ BilliardsManagerment/Services/Implementations/RoleService.cs
@@ -0,0 +1,21 @@
+﻿using BilliardsManagerment.Entities;
+using BilliardsManagerment.Services.Interfaces;
+
+namespace BilliardsManagerment.Services.Implementations
+{
+    public class RoleService : IRoleService
+    {
+        private readonly BilliardsManagementContext _context;
+            
+        public RoleService(BilliardsManagementContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Role> GetRoles()
+        {
+            var roles = _context.Roles.ToList();
+            return roles;   
+        }
+    }
+}
---
+++ BilliardsManagerment/Services/Interfaces/IRoleService.cs
@@ -0,0 +1,9 @@
+﻿using BilliardsManagerment.Entities; 
+
+namespace BilliardsManagerment.Services.Interfaces
+{
+    public interface IRoleService
+    {
+        ICollection<Role>GetRoles();
+    }
+}
